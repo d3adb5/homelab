6a0e2195bfeebbce508af3c1ab0a51c0df450fc4
d3adb5 <me@d3adb5.net>
2024-09-21 14:11:51 -0700

    feat(dns): add subdomain records for hosts
    Add subdomain records for each host that is within this domain.

--- dns/variables.tf
+++ dns/variables.tf
@@ -8,3 +8,12 @@ variable "zone_name" {
   description = "Name of the DNS zone to manage"
   type        = string
 }
+
+variable "hosts" {
+  description = "Map of hosts to create subdomain records for"
+
+  type = map(object({
+    ipv4 = string
+    ipv6 = optional(string)
+  }))
+}
