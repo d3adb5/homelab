91790fc613ea898438fae1264f9fc12e40ffaedb
d3adb5 <me@d3adb5.net>
2024-09-21 13:53:58 -0700

    feat(dns): add github pages records for charts subdomain
    Add records for the charts subdomain targeting GitHub Pages. This is
    because my custom charts are hosted on GitHub Pages.

--- dns/github_pages.tf
+++ dns/github_pages.tf
@@ -1,29 +1,47 @@
-resource "cloudflare_record" "github_pages_ipv4" {
-  for_each = toset([
+locals {
+  github_pages_ipv4 = [
     "185.199.108.153",
     "185.199.109.153",
     "185.199.110.153",
     "185.199.111.153",
-  ])
+  ]
 
-  zone_id = data.cloudflare_zone.this.id
-
-  name    = "@"
-  type    = "A"
-  content = each.key
-}
-
-resource "cloudflare_record" "github_pages_ipv6" {
-  for_each = toset([
+  github_pages_ipv6 = [
     "2606:50c0:8000:0:0:0:0:153",
     "2606:50c0:8001:0:0:0:0:153",
     "2606:50c0:8002:0:0:0:0:153",
     "2606:50c0:8003:0:0:0:0:153",
-  ])
+  ]
+}
 
-  zone_id = data.cloudflare_zone.this.id
+resource "cloudflare_record" "github_pages_ipv4" {
+  for_each = toset(local.github_pages_ipv4)
+  zone_id  = data.cloudflare_zone.this.id
+  name     = "@"
+  type     = "A"
+  content  = each.key
+}
 
-  name    = "@"
-  type    = "AAAA"
-  content = each.key
+resource "cloudflare_record" "github_pages_ipv4_charts" {
+  for_each = toset(local.github_pages_ipv4)
+  zone_id  = data.cloudflare_zone.this.id
+  name     = "charts"
+  type     = "A"
+  content  = each.key
+}
+
+resource "cloudflare_record" "github_pages_ipv6" {
+  for_each = toset(local.github_pages_ipv6)
+  zone_id  = data.cloudflare_zone.this.id
+  name     = "@"
+  type     = "AAAA"
+  content  = each.key
+}
+
+resource "cloudflare_record" "github_pages_ipv6_charts" {
+  for_each = toset(local.github_pages_ipv6)
+  zone_id  = data.cloudflare_zone.this.id
+  name     = "charts"
+  type     = "AAAA"
+  content  = each.key
 }
